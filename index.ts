9b22634b33c065713a75435547c91f0710b02ef7
eakarpov <eakarpov@yandex.ru>
2018-12-21 01:15:42 +0300

    types

--- index.ts
+++ index.ts
@@ -2,45 +2,45 @@ import {
     sigma,
 } from './objects';
 import {
-  Sigma,
-  ObjectType,
-  Call,
-  Lambda,
-  MethodUpdate,
-  FieldUpdate,
-  Int,
-  Float,
-  Expression,
-  CutExpr,
-  Method,
-  ReturnValue,
-  Field,
-  ExprBody,
-  Function,
-  Parameter,
-  Add,
-  Value,
-  Argument,
+    Sigma,
+    ObjectType,
+    Call,
+    Lambda,
+    MethodUpdate,
+    FieldUpdate,
+    Int,
+    Float,
+    Expression,
+    CutExpr,
+    Method,
+    ReturnValue,
+    Field,
+    ExprBody,
+    Function,
+    Parameter,
+    Add,
+    Value,
+    Argument, Type,
 } from './types';
-import { context, ContextType } from './dictionary';
+import {context, ContextType} from './dictionary';
 
 
-function findMethod(object: ObjectType, name: string): Method|Field|undefined {
+function findMethod(object: ObjectType, name: string): Method | Field | undefined {
     for (const method of object.props) {
         if (method.name === name) return method;
     }
 }
 
 function setMethod(ctx: ObjectType, name: Field | Method): ObjectType {
-  const res = new ObjectType([]);
+    const res = new ObjectType([]);
     for (let i = 0; i < ctx.props.length; i++) {
-      if (ctx.props[i].name === name.name) {
-          res.props.push(name);
-      } else {
-          res.props.push(ctx.props[i]);
-      }
-  }
-  return res;
+        if (ctx.props[i].name === name.name) {
+            res.props.push(name);
+        } else {
+            res.props.push(ctx.props[i]);
+        }
+    }
+    return res;
 }
 
 function evalLambda(body: Lambda, args?: Argument[]): ReturnValue {
@@ -49,7 +49,7 @@ function evalLambda(body: Lambda, args?: Argument[]): ReturnValue {
             context.set(arg.name, args[i]);
         }
     });
-  return evalExpr(body.body);
+    return evalExpr(body.body);
 }
 
 function evalBody(body: Value, args?: Argument[]): ReturnValue {
@@ -79,13 +79,13 @@ function evalParam(param: Parameter): ReturnValue {
         }
         throw new Error('Not a valid type');
     } else {
-      if (param instanceof ObjectType) {
-        return param; 
-      }
-      if (param instanceof Int || param instanceof Float) {
-        return param.value;
-      }
-      return context.get(param.ctx as string).pop() as ReturnValue;
+        if (param instanceof ObjectType) {
+            return param;
+        }
+        if (param instanceof Int || param instanceof Float) {
+            return param.value;
+        }
+        return context.get(param.ctx as string).pop() as ReturnValue;
     }
 }
 
@@ -98,17 +98,17 @@ function evalExprBodies(body: ExprBody[]): ReturnValue {
 }
 
 function getNewMethod(method: Method, ctx: ObjectType): Method {
-  if (method.body instanceof Expression) {
-      const body = evalExprBodies(method.body.args);
-      if (body instanceof ObjectType) {
-        return new Method(method.name, method.type, method.ctx, body);
-      } else {
-          throw new Error('ObjectType expected');
-      }
-  } else if (method.body instanceof Lambda) {
-      return new Method(method.name, method.type, ctx, method.body)
-  }
-  return method;
+    if (method.body instanceof Expression) {
+        const body = evalExprBodies(method.body.args);
+        if (body instanceof ObjectType) {
+            return new Method(method.name, method.type, method.ctx, body);
+        } else {
+            throw new Error('ObjectType expected');
+        }
+    } else if (method.body instanceof Lambda) {
+        return new Method(method.name, method.type, ctx, method.body)
+    }
+    return method;
 }
 
 function c(value: ReturnValue): number {
@@ -121,34 +121,66 @@ function c(value: ReturnValue): number {
     return 0;
 }
 
+function validateArgs(type: Array<string|Type>, mtd: Call) {
+    const innerArgs = [...mtd.args];
+    // correlate each type arg with args array
+    for (const t of type) {
+        if (t === 'Obj') continue;
+        if (t === 'Int') {
+            const arg = innerArgs.pop();
+            if (arg === void 0) throw new Error(`not enough arguments for method ${mtd.name}`);
+            if (!(arg instanceof Int && Number.isInteger(arg.value))) {
+                throw new Error(`${arg} is not of a required [${t}] type`);
+            }
+        }
+        if (t === 'Real') {
+            const arg = innerArgs.pop();
+            if (arg === void 0) throw new Error(`not enough arguments for method ${mtd.name}`);
+            if (!(arg instanceof Float && !Number.isNaN(Number.parseFloat(arg.value.toString())))) {
+                throw new Error(`${arg} is not of a required [${t}] type`);
+            }
+        }
+    }
+}
+
+function validateType(type: Array<string|Type>, args: Array<string|Type>) {
+    for (let i = 0; i < type.length; i++) {
+        if (type[i] !== args[i]) throw new Error(`type [${args[i]}] does not equal to [${type[i]}] type`);
+    }
+}
+
 function evalExprBody(body: ExprBody): ReturnValue {
     if (body instanceof FieldUpdate) {
         const ctx = context.get(body.ctx || '_').pop();
         if (!(ctx instanceof ObjectType)) throw new Error('Object type is required here');
         const method = findMethod(ctx, body.propName);
         if (method) {
-          let bd = new Int(0);
-          if (body.value instanceof Expression) {
-              bd = b(evalExpr(body.value));
-          }
-          const field = new Field(method.name, method.type, bd);
-          return setMethod(ctx, field);
+            let bd = new Int(0);
+            if (body.value instanceof Expression) {
+                bd = b(evalExpr(body.value));
+            }
+            validateType([...method.type.args].slice(0, method.type.args.length - 1), method.type.args);
+            const field = new Field(method.name, method.type, bd);
+            validateType([method.type.args[method.type.args.length - 1]], [method.type.args[method.type.args.length - 1]]);
+            return setMethod(ctx, field);
         } else {
             throw new Error('Method not found');
         }
     }
     if (body instanceof MethodUpdate) {
-      const ctx = context.get(body.ctx || '_').pop();
-      if (!(ctx instanceof ObjectType)) throw new Error('Object type is required here');
-      const method: Method = findMethod(ctx, body.propName) as Method;
-      const newMethod = getNewMethod(method, ctx);
-      return setMethod(ctx, newMethod);
+        const ctx = context.get(body.ctx || '_').pop();
+        if (!(ctx instanceof ObjectType)) throw new Error('Object type is required here');
+        const method: Method = findMethod(ctx, body.propName) as Method;
+        validateType([...method.type.args].slice(0, method.type.args.length - 1), method.type.args);
+        const newMethod = getNewMethod(method, ctx);
+        validateType([method.type.args[method.type.args.length - 1]], [method.type.args[method.type.args.length - 1]]);
+        return setMethod(ctx, newMethod);
     }
     if (body instanceof Function) {
-       if (body.operand instanceof Add) {
-           return c(evalParam(body.arg1)) + c(evalParam(body.arg2));
-       }
-       return 0;
+        if (body.operand instanceof Add) {
+            return c(evalParam(body.arg1)) + c(evalParam(body.arg2));
+        }
+        return 0;
     }
     if (body instanceof Parameter) {
         return evalParam(body);
@@ -166,17 +198,17 @@ function evalExpr(expr: Expression): ReturnValue {
 }
 
 function b(body: ReturnValue) {
-  if (typeof body === 'number') {
-      if (parseInt(body.toString()) === body) {
-          return new Int(body);
-      } else {
-          return new Float(body);
-      }
-  }
-  if (body instanceof ObjectType) {
-      throw new Error('Not implemented!');
-  }
-  return body;
+    if (typeof body === 'number') {
+        if (parseInt(body.toString()) === body) {
+            return new Int(body);
+        } else {
+            return new Float(body);
+        }
+    }
+    if (body instanceof ObjectType) {
+        throw new Error('Not implemented!');
+    }
+    return body;
 }
 
 function methodCall(methodCall: Call): ReturnValue {
@@ -196,14 +228,19 @@ function methodCall(methodCall: Call): ReturnValue {
     if (!(ctx instanceof ObjectType)) throw new Error('Not a context object');
     const methodToExecute = findMethod(ctx, methodCall.name);
     if (methodToExecute) {
-      if (methodToExecute instanceof Method) {
-          if (typeof methodToExecute.ctx === 'string') {
-              context.set('_default', methodToExecute.ctx);
-            context.set(methodToExecute.ctx, ctx);
-          }
-        return evalBody(methodToExecute.body, args);
-      }
-      return evalBody(methodToExecute.body);
+        const type = methodToExecute.type.args;
+        validateArgs([...type].slice(0, type.length - 1), methodCall);
+        if (methodToExecute instanceof Method) {
+            if (typeof methodToExecute.ctx === 'string') {
+                context.set('_default', methodToExecute.ctx);
+                context.set(methodToExecute.ctx, ctx);
+            }
+            const res = evalBody(methodToExecute.body, args);
+            const outputType = type[type.length - 1];
+            validateArgs([outputType], { ...methodToExecute, args: [res] } as any as Call);
+            return res;
+        }
+        return evalBody(methodToExecute.body);
     } else {
         throw new Error('Method not found');
     }
@@ -227,13 +264,13 @@ function resultOfCall(call: CutExpr): ReturnValue {
 }
 
 function evalMain(sigma: Sigma): string {
-    const ctx =  evalSigma(sigma.objectType, sigma.call);
+    const ctx = evalSigma(sigma.objectType, sigma.call);
     context.set('_', ctx);
     const result = resultOfCall(sigma.call);
     return result.toString();
 }
 
-void function() {
+void function () {
     console.log(sigma);
     console.log(evalMain(sigma));
 }();
